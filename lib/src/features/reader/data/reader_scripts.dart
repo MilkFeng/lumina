2aadc3ee6da1b7dd989e2fe9e011effc85ce24a2
MilkFeng <milkfeng666@gmail.com>
2026-02-10 17:17:10 +0800

    Add convertToColumnBreak function for improved CSS handling in polyfillCss

--- lib/src/features/reader/data/reader_scripts.dart
+++ lib/src/features/reader/data/reader_scripts.dart
@@ -279,6 +279,20 @@ function calculateScrollLeft(iframe, pageIndex) {
   return scrollLeft;
 }
 
+function convertToColumnBreak(value) {
+  switch (value) {
+    case 'page':
+    case 'right':
+    case 'left':
+      return 'always';
+    case 'avoid':
+      return 'avoid';
+    case 'auto':
+    default:
+      return 'auto';
+  }
+}
+
 function polyfillCss(doc) {
   for (var i = 0; i < doc.styleSheets.length; i++) {
     var sheet = doc.styleSheets[i];
@@ -291,24 +305,20 @@ function polyfillCss(doc) {
         if (rule.type === 1) {
           var style = rule.style;
           
-          if (style.breakBefore && style.breakBefore !== 'auto') {
-            style.breakBefore = 'column';
-            style.webkitColumnBreakBefore = 'always';
+          if (style.breakBefore) {
+            style.webkitColumnBreakBefore = convertToColumnBreak(style.breakBefore);
           }
           
-          if (style.pageBreakBefore && style.pageBreakBefore !== 'auto') {
-            style.webkitColumnBreakBefore = 'always';
-            style.breakBefore = 'column';
+          if (style.pageBreakBefore) {
+            style.webkitColumnBreakBefore = convertToColumnBreak(style.pageBreakBefore);
           }
 
           if (style.breakAfter && style.breakAfter !== 'auto') {
-            style.breakAfter = 'column';
-            style.webkitColumnBreakAfter = 'always';
+            style.webkitColumnBreakAfter = convertToColumnBreak(style.breakAfter);
           }
 
           if (style.pageBreakAfter && style.pageBreakAfter !== 'auto') {
-            style.webkitColumnBreakAfter = 'always';
-            style.breakAfter = 'column';
+            style.webkitColumnBreakAfter = convertToColumnBreak(style.pageBreakAfter);
           }
         }
       }
